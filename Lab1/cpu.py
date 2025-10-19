9a4bcd9e9e197da14d19abb0087cdd175e655007
Hbrtjm <hubertm@remitly.com>
2025-10-19 19:57:30 +0200

    Added code for first lab

---
+++ Lab1/cpu.py
@@ -0,0 +1,90 @@
+
+class Cpu:
+  def __init__(self, mem, undefined_cb=None):
+    self.mem = mem
+    self.undefined_cb = undefined_cb
+    self.reset()
+    self.__opcode_h = 0
+    self.__opcode_l = 0
+  
+  def reset(self):
+    self.state = 0
+    self.mem_address = 0
+    self.mem_data_r = 0
+    self.mem_data_w = 0
+    self.pc = 0
+    self.r0 = 0
+    self.r1 = 0
+    self.opcode = 0
+    
+  
+  def next_cycle(self):
+    if self.state == 0:
+      self.state = 1
+      self.mem_address = self.pc
+      self.mem_data_r = self.mem[self.mem_address]
+    elif self.state == 1:
+      self.state = 2
+      self.opcode = self.mem_data_r
+      self.pc = self.pc + 1
+      
+    elif self.state == 2:
+      self.state = 3
+      self.__opcode_h = (self.opcode & 0xF0) >> 4
+      self.__opcode_l = self.opcode & 0x0F
+      # jmp and data processing (opcode bit 7 is 0)
+      if self.__opcode_h == 0x0:
+        self.pc = self.__opcode_l
+      elif self.__opcode_h == 0x2:
+        self.r0 = self.r0 + self.r1
+      elif self.__opcode_h == 0x3:
+        self.r1 = self.r0 * self.r1 
+      elif self.__opcode_h == 0x8:
+        self.r0 = self.__opcode_l
+      elif self.__opcode_h == 0x9:
+        self.r1 = self.__opcode_l
+      elif self.__opcode_h == 0xA:
+        self.r0 = self.r1
+      elif self.__opcode_h == 0xB:
+        self.r1 = self.r0
+      # load and store (opcode bit 7 is 1)
+      elif self.__opcode_h == 0xC:
+        self.mem_address = self.__opcode_l
+      elif self.__opcode_h == 0xD:
+        self.mem_address = self.__opcode_l
+      elif self.__opcode_h == 0xE:
+        self.mem_address = self.__opcode_l
+        self.mem_data_w = self.r0
+      elif self.__opcode_h == 0xF:
+        self.mem_address = self.__opcode_l
+        self.mem_data_w = self.r1
+      else:
+        self.undefined_cb(self.mem_address, self.__opcode_h)
+        
+    elif self.state == 3:
+      self.state = 0
+      if self.__opcode_h == 0xC:
+        self.r0 = self.mem[self.mem_address]
+      elif self.__opcode_h == 0xD:
+        self.r1 = self.mem[self.mem_address]
+      elif self.__opcode_h == 0xE:
+        self.mem[self.mem_address] = self.mem_data_w
+      elif self.__opcode_h == 0xF:
+        self.mem[self.mem_address] = self.mem_data_w
+      # else:
+      #   self.undefined_cb(self.mem_address, self.__opcode_h)
+        
+    else:
+      print(f'something went wrong, state should not be >3')
+    self.debug_dump()
+    
+
+  def __intlist_to_hex_str(self,intlist):
+    return f"{' '.join(f'{n:02X}' for n in intlist)}"
+
+  def debug_dump(self):
+    print(f"c={self.state:X} r0={self.r0:X} r1={self.r1:X} \
+pc={self.pc:X} A={self.mem_address:X} R={self.mem_data_r:02X} \
+W={self.mem_data_w:02X} M: {self.__intlist_to_hex_str(self.mem)}")
+    
+
---
+++ Lab1/scs.py
@@ -0,0 +1,87 @@
+import sys
+import termios
+import fcntl
+import os
+from cpu import Cpu
+
+def undefined(address, data):
+    print(f'Undefined instruction at address 0x{address:02X}, {data:02X}')
+
+def init_nonblocking_input():
+    fd = sys.stdin.fileno()
+
+    # Save original terminal settings
+    original_termios = termios.tcgetattr(fd)
+    new_termios = termios.tcgetattr(fd)
+
+    # Turn off canonical mode and echo
+    new_termios[3] &= ~(termios.ICANON | termios.ECHO)
+    termios.tcsetattr(fd, termios.TCSANOW, new_termios)
+
+    # Set stdin to non-blocking
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+    return original_termios
+
+def restore_input(original_termios):
+    fd = sys.stdin.fileno()
+    termios.tcsetattr(fd, termios.TCSANOW, original_termios)
+
+def read_key():
+    try:
+        return sys.stdin.read(1)
+    except IOError:
+        return None
+
+# 1) Wyczyścić zawartość rejestrów R0 i R1 wpisując do nich wartość 0,
+# 2) Wyczyścić zawartość komórki pamięci o adresie 14 wpisując do niej wartość 0,
+# 3) Do rejestru R0 wpisać liczbę 0x6
+# 4) Dodać do rejestru R0 wartość przechowywaną w pamięci RAM pod adresem 13,
+# 5) Umieścić zawartość rejestru R0 w pamięci RAM w komórce o adresie 14,
+# 6) Powrócić do adresu 0, aby zapętlić się        
+
+base_program = [\
+  0x80, \
+  0x90, \
+  0x81, \
+  0x92, \
+  0xA0, \
+  0x00, \
+  0x00, \
+  0x00, \
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+load_store_program = [\
+  0x80, \
+  0x90, \
+  0x86, \
+  0xCD, \
+  0xEE, \
+  0x83, \
+  0x92, \
+  0x30, \
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00]
+
+
+if __name__ == "__main__":
+    cpu = Cpu(load_store_program,undefined)
+
+    print("Press any key ('q' to quit, space for clock, d for debug dump)...")
+
+    original_settings = init_nonblocking_input()
+    try:
+        while True:
+            key = read_key()
+            if key:
+                # print(f"Key pressed: {repr(key)}") # uncomment to see what's pressed
+                if key == 'q':
+                    break
+                if key == 'd':
+                    cpu.debug_dump()
+                if key == ' ':
+                    cpu.next_cycle()
+    finally:
+        restore_input(original_settings)
+        print("\nExit.")
+
